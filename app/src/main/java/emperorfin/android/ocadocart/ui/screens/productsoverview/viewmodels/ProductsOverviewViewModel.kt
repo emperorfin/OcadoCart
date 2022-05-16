51a32be27dc26d4d90ff2423473c986e8d88b737
Francis Nwokelo (emperorfin) <emperorfin4@gmail.com>
2022-05-16 23:01:34 +0100

    [UPDATED] The products overviews list UI screen now shows sample data of clickable list of products overviews from the Room database via a "local data source" (pattern).

--- app/src/main/java/emperorfin/android/ocadocart/ui/screens/productsoverview/viewmodels/ProductsOverviewViewModel.kt
+++ app/src/main/java/emperorfin/android/ocadocart/ui/screens/productsoverview/viewmodels/ProductsOverviewViewModel.kt
@@ -6,12 +6,21 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import emperorfin.android.ocadocart.data.datasources.local.frameworks.room.AppRoomDatabase
+import emperorfin.android.ocadocart.data.datasources.local.frameworks.room.entitysources.ProductOverviewLocalDataSourceRoom
+import emperorfin.android.ocadocart.domain.exceptions.ProductOverviewFailure.LocalProductOverviewError
+import emperorfin.android.ocadocart.domain.exceptions.ProductOverviewFailure.ListNotAvailableLocalProductOverviewError
+import emperorfin.android.ocadocart.domain.models.ProductOverviewModel
 import emperorfin.android.ocadocart.domain.models.mappers.ProductOverviewModelMapper
 import emperorfin.android.ocadocart.ui.events.outputs.EventDataImpl
 import emperorfin.android.ocadocart.ui.screens.productsoverview.enums.ProductsOverviewRequestStatus
 import emperorfin.android.ocadocart.ui.uimodels.ProductOverviewUiModel
 import emperorfin.android.ocadocart.ui.uimodels.mappers.ProductOverviewUiModelMapper
 import emperorfin.android.ocadocart.ui.utils.ProductsOverviewsSampleDataGeneratorUtil
+import emperorfin.android.ocadocart.domain.uilayer.events.outputs.ResultData
+import emperorfin.android.ocadocart.domain.uilayer.events.outputs.ResultData.Success
+import emperorfin.android.ocadocart.domain.uilayer.events.outputs.ResultData.Error
+import emperorfin.android.ocadocart.domain.uilayer.events.outputs.succeeded
+import emperorfin.android.ocadocart.ui.events.inputs.productoverview.None
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
@@ -48,7 +57,9 @@ class ProductsOverviewViewModel(
         // Option 1 of 4 (SAMPLE DATA)
 //        generateProductsOverviewsSampleData()
         // Option 2 of 4 (SAMPLE DATA)
-        getDatabaseProductsOverviewsSampleDataWithoutLocalDataSource()
+//        getDatabaseProductsOverviewsSampleDataWithoutLocalDataSource()
+        // Option 3 of 4 (SAMPLE DATA)
+        getDatabaseProductsOverviewsSampleDataViaLocalDataSource()
     }
 
     fun openProductDetails(productOverview: ProductOverviewUiModel){
@@ -90,5 +101,33 @@ class ProductsOverviewViewModel(
             _requestStatus.value = ProductsOverviewRequestStatus.NO_DATA
         else
             _requestStatus.value = ProductsOverviewRequestStatus.DONE
+    }
+
+    private fun getDatabaseProductsOverviewsSampleDataViaLocalDataSource() = viewModelScope.launch{
+        _requestStatus.value = ProductsOverviewRequestStatus.LOADING
+
+        val localDataSourceRoom: ProductOverviewLocalDataSourceRoom = ProductOverviewLocalDataSourceRoom(
+            applicationContext,
+            AppRoomDatabase.getInstance(applicationContext).mProductOverviewDao
+        )
+
+        val params = None()
+        val resultData: ResultData<List<ProductOverviewModel>> =
+            localDataSourceRoom.getProductsOverviews(params)
+
+        if (resultData is Error && (resultData.failure is LocalProductOverviewError ||
+                    resultData.failure is ListNotAvailableLocalProductOverviewError)){
+            _requestStatus.value = ProductsOverviewRequestStatus.NO_DATA
+        }else if (resultData.succeeded){
+            val modelProductsOverviews = (resultData as Success).data
+
+            val productOverviewUiModelMapper = ProductOverviewUiModelMapper()
+
+            _productsOverviews.value = modelProductsOverviews.map {
+                productOverviewUiModelMapper.transform(it)
+            }
+
+            _requestStatus.value = ProductsOverviewRequestStatus.DONE
+        }
     }
 }
